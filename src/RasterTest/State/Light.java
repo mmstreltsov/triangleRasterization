babc8f4dec5c05e356f7e66d51dab86b0b8c21ff
mestreltsov <mestreltsov@edu.hse.ru>
2023-04-28 21:04:15 +0300

    implement Light

--- src/RasterTest/State/Light.java
+++ src/RasterTest/State/Light.java
@@ -1,5 +1,34 @@
 package RasterTest.State;
 
+import RasterTest.State.Math.Vector3D;
+
 public class Light {
 
+    private static Light light;
+    private Vector3D direction;
+
+    public static Light fabric() {
+        if (light == null) {
+            light = new Light();
+        }
+        return light;
+    }
+
+    public static Light fabric(Vector3D direction) {
+        if (light == null) {
+            light = new Light(direction);
+        }
+        else {
+            light.direction = direction;
+        }
+        return light;
+    }
+
+    private Light() {
+        direction = new Vector3D(0, 0, 1);
+    }
+
+    private Light(Vector3D vec) {
+        this.direction = vec;
+    }
 }
