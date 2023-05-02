3b04b063d81d7f62bd8a20dc4d2964fa9c06ec1d
mestreltsov <mestreltsov@edu.hse.ru>
2023-05-02 22:11:12 +0300

    add 2 directions (OY)

--- src/RasterTest/CameraAnimation/CameraAnimTest.java
+++ src/RasterTest/CameraAnimation/CameraAnimTest.java
@@ -3,12 +3,17 @@ package RasterTest.CameraAnimation;
 import RasterTest.State.Animation.Animate;
 
 public class CameraAnimTest {
+    public static char[] getChars() {
+        return new char[]{'w', 'a', 's', 'd', 'q', 'e', ',', 'l', '.', ';', '/', '\''};
+    }
     public static void testing(char c) {
         switch (c) {
             case 'w' -> Animate.animCameraTransl(0, 0, 1);
             case 'a' -> Animate.animCameraTransl(-1, 0, 0);
             case 's' -> Animate.animCameraTransl(0, 0, -1);
             case 'd' -> Animate.animCameraTransl(1, 0, 0);
+            case 'q' -> Animate.animCameraTransl(0, 1, 0);
+            case 'e' -> Animate.animCameraTransl(0, -1, 0);
             case ',' -> Animate.animCameraRotate(-1, 0, 0);
             case 'l' -> Animate.animCameraRotate(1, 0, 0);
             case '.' -> Animate.animCameraRotate(0, -1, 0);
--- src/RasterTest/CameraAnimation/KeyChecker.java
+++ src/RasterTest/CameraAnimation/KeyChecker.java
@@ -2,12 +2,10 @@ package RasterTest.CameraAnimation;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.KeyEvent;
 
 public class KeyChecker {
     public static void init(InputMap inputMap, ActionMap actionMap) {
-        char[] chars = new char[]{'w', 'a', 's', 'd', ',', 'l', '.', ';', '/', '\''};
-
+        char[] chars = CameraAnimTest.getChars();
         for (char c : chars) {
             inputMap.put(KeyStroke.getKeyStroke(c), String.valueOf(c));
             actionMap.put(String.valueOf(c), new AbstractAction() {
