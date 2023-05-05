db477145ac3afe722b2ea953ed58e779a248de5d
mestreltsov <mestreltsov@edu.hse.ru>
2023-05-05 17:26:24 +0300

    make documentation. Fix bags

--- src/RasterTest/Render.java
+++ src/RasterTest/Render.java
@@ -8,21 +8,36 @@ import RasterTest.State.Scene;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Обертка над RenderState с возможностью рендерить сцену
+ */
 public class Render {
+
+    /**
+     * Все объекты лежат здесь
+     */
     private final RenderState renderState = new RenderState();
 
+    /**
+     * Функция вставляет в коллекцию объектов для рендера
+     * @param renderObject объект класса RenderObject
+     */
     public void insert(RenderObject renderObject) {
         renderState.getRenderObjects().add(renderObject);
     }
 
+    /**
+     * Функция генерирует RenderObject и вставляет в коллекцию объектов для рендера
+     * @param scene Сцена
+     * @param model Модель
+     */
     public void insert(Scene scene, Model model) {
         renderState.getRenderObjects().add(new RenderObject(scene, model));
     }
 
     /**
-     *
-     *
-     * Добавлена фича (В совокупности с методом Scene.getting2DCoordinate):
+     * Рендер кадра. Вызывает init для всех объектов коллекции. Собирает Треугольники для канваса
+     * !Добавлена фича (В совокупности с методом Scene.getting2DCoordinate):
      * !Когда камера слишком близко к объекту => камера уезжает на нужное расстояние, и весь рендер пересобирается
      * @return
      */
