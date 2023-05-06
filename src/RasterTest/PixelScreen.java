18e455744e6c35aa8f7428b79b1102a882c774ed
godRa1deN <andrey_2468@mail.ru>
2023-05-06 21:27:08 +0300

    add double buffer

--- src/RasterTest/PixelScreen.java
+++ src/RasterTest/PixelScreen.java
@@ -1,11 +1,9 @@
 package RasterTest;
 
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.awt.image.WritableRaster;
-import javax.swing.JFrame;
+import java.util.Arrays;
 
 public class PixelScreen extends Canvas {
     public static final int resolutionX = 640;
@@ -15,40 +13,58 @@ public class PixelScreen extends Canvas {
     private final int[] blackColorPixel = new int[]{0, 0, 0};
 
 
+
     public PixelScreen() {
         this.bi = new BufferedImage(resolutionX, resolutionY, BufferedImage.TYPE_INT_RGB);
         this.wr = this.bi.getRaster();
         this.setSize(new Dimension(resolutionX, resolutionY));
     }
 
-    public void fillPixels(int[] color) {
-        for(int x = 0; x < resolutionX; x++) {
-            for(int y = 0; y < resolutionY; y++) {
-                this.wr.setPixel(x, y, color);
-            }
-        }
-        this.paint(this.getGraphics());
-    }
 
-    public void drawLine(int x1, int x2, int y, int[] color) {
-        for (int x = x1; x < x2; x++) {
-            this.wr.setPixel(x, y, color);
-        }
-    }
-
-    public void Clear() {
+    public void ClearBuffer(int[][][] buffer) {
         for(int x = 0; x < resolutionX; x++) {
-            for(int y = 0; y < resolutionY; y++) {
-                this.wr.setPixel(x, y, blackColorPixel);
+            for (int y = 0; y < resolutionY; y++) {
+                buffer[x][y] = blackColorPixel;
             }
         }
     }
-    public void drawTriangle(Triangle triangle, int[] color) {
+
+//    public void drawTriangle(Triangle triangle, int[] color) {
+//        int[][] a = triangle.findCoordinates();
+//        for (int[] ints : a) {
+//            drawLine(ints[0], ints[1], ints[2], color);
+//        }
+//    }
+
+//    public void drawLine(int x1, int x2, int y, int[] color) {
+//        for (int x = x1; x < x2; x++) {
+//            this.wr.setPixel(x, y, color);
+//        }
+//    }
+
+    public int[][][] addTriangleToBuffer(Triangle triangle, int[] color, int[][][] buffer) {
         int[][] a = triangle.findCoordinates();
         for (int[] ints : a) {
-            drawLine(ints[0], ints[1], ints[2], color);
+            buffer = addLineToBuffer(ints[0], ints[1], ints[2], color, buffer);
+        }
+        return buffer;
+    }
+
+    public int[][][] addLineToBuffer(int x1, int x2, int y, int[] color, int[][][] buffer) {
+        for (int x = x1; x < x2; x++) {
+            buffer[x][y] = color;
+        }
+        return buffer;
+    }
+
+    public void addBufferToCanvas(int[][][] buffer) {
+        for(int x = 0; x < resolutionX; x++) {
+            for(int y = 0; y < resolutionY; y++) {
+                this.wr.setPixel(x, y, buffer[x][y]);
+            }
         }
     }
+
 
     public void drawCanvas() {
         this.paint(this.getGraphics());
