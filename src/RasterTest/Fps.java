1c24ddfd88960f2ace886a277f36353e9136702d
godRa1deN <andrey_2468@mail.ru>
2023-05-12 15:05:27 +0300

    connect threads to GUI + code refactor

---
+++ src/RasterTest/Fps.java
@@ -0,0 +1,22 @@
+package RasterTest;
+
+public class Fps {
+    private long lastFpsCheck = 0;
+    private int totalFrames = 0;
+
+    Fps() {
+        this.lastFpsCheck = 0;
+        this.totalFrames = 0;
+    }
+
+    public int fps() {
+        if (System.nanoTime() - lastFpsCheck > 1_000_000_000) {
+            lastFpsCheck = System.nanoTime();
+            int currentFps = totalFrames;
+            totalFrames = 0;
+            return currentFps;
+        }
+        totalFrames++;
+        return -1;
+    }
+}
